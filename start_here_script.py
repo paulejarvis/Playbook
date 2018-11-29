0ad75315595586deb31da3de486a79287c73369b
Paul Jarvis <paul.e.jarvis@gmail.com>
2018-11-29 15:45:42 -0800

    added different fields to include; not yet coded

--- start_here_script.py
+++ start_here_script.py
@@ -5,3 +5,31 @@ print("Welcome to Playbook. We're going to ask some questions to understand\
 
 name = raw_input("What is the name of your organization?")
 inc_date
+
+
+
+# other fields to capture
+
+
+Incorporation Date
+Incorporation Location
+Business Structure
+    Sole proprietor (taxes paid through personal income tax return of owner);
+    Partnership (taxes paid through personal income tax return of owners);
+    Corporation (taxes paid by corporation);
+    S corporation (taxes paid through personal income tax returns, but owners must be paid "reasonable compensation");
+    Limited Liability Company (LLC) (hybrid legal structure, taxes paid like a partnership, but receive personal liability shield);
+    Non-profit (may be tax exempt).
+Employer Identification Number (EIN)
+    Licenses / Permits (according to category)
+    Agriculture - the U.S. Department of Agriculture issues permits for business involved in the importation and interstate transportation of animals, animal products, biologics, biotechnology and plants.
+    Alcoholic beverages - manufacturers, wholesalers, importers and retailers of alcohol beverages must register, pay special taxes and maintain certain records with the Alcohol and Tobacco Tax and Trade Bureau(ATTB).
+    Aviation - the Federal Aviation Administration (FAA) issues licenses for pilots, aircraft, airports, and medical aviation services.
+    Environmental - the Environmental Protection Agency (EPA) imposes environment regulations at the federal level. If your business buys or sells land that is contaminated, you'll need to go through the EPA to get rid of the contaminants.
+    Firearms, ammunition and explosives - manufacturers, dealers and importers of firearms and ammunitions are controlled by the Bureau of Alcohol, Tobacco and Firearms (ATF).
+    Fish and wildlife - businesses engaged in any wildlife activity, including the importation of any wildlife or their derivative products, must receive a permit from the U.S. Fish and Wildlife Service.
+    Investment Advising - the Securities and Exchange Commission (SEC) requires those who professionally counsel on investments to be licensed.
+    Mining and drilling - Minerals Management Service (MMS), a bureau in the U.S. Department of the Interior, is the federal agency that manages the nation's natural gas, oil and other mineral resources.
+    Radio and television broadcasting - If your business broadcasts information by radio, television, wire, satellite and cable, you are required to obtain a license from the Federal Communications Commission (FCC). The FCC doesn't regulate the internet.
+    Interstate ground transportation - you must register if your business will cross state lines.
+    Maritime transportation - the Federal Maritime Commission issues licenses for businesses that transport freight or passengers on the ocean.
