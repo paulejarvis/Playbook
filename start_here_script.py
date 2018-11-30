eec708e87c795f5d2a070cf07709ef3ea4f4c1ca
Paul Jarvis <paul.e.jarvis@gmail.com>
2018-11-29 16:40:12 -0800

    capturing details on different types of compliance to track

--- start_here_script.py
+++ start_here_script.py
@@ -10,6 +10,11 @@ inc_date
 
 # other fields to capture
 
+Local >> State >> Federal >> International
+Tax
+
+
+
 
 Incorporation Date
 Incorporation Location
@@ -20,6 +25,10 @@ Business Structure
     S corporation (taxes paid through personal income tax returns, but owners must be paid "reasonable compensation");
     Limited Liability Company (LLC) (hybrid legal structure, taxes paid like a partnership, but receive personal liability shield);
     Non-profit (may be tax exempt).
+
+Business structure pt 2
+- subsidiaries / branches
+
 Employer Identification Number (EIN)
     Licenses / Permits (according to category)
     Agriculture - the U.S. Department of Agriculture issues permits for business involved in the importation and interstate transportation of animals, animal products, biologics, biotechnology and plants.
@@ -33,3 +42,37 @@ Employer Identification Number (EIN)
     Radio and television broadcasting - If your business broadcasts information by radio, television, wire, satellite and cable, you are required to obtain a license from the Federal Communications Commission (FCC). The FCC doesn't regulate the internet.
     Interstate ground transportation - you must register if your business will cross state lines.
     Maritime transportation - the Federal Maritime Commission issues licenses for businesses that transport freight or passengers on the ocean.
+
+Compliance regulations to build out
+- GDPR
+- SOC2
+- GDPR
+- state-specific regulations
+
+Some laws apply to everyone
+* child labor laws
+* OSHA - employee safety
+* Immigration reform and control act (form I-9)
+* independent contractor rules from the IRS
+* Military service leave
+* Display of appropriate noties and posters
+* Privacy
+* Fair labor standards act
+* Workers compensation
+
+
+ADA Amendments Act – 15 Employees
+    - The ADA Amendments Act (ADAAA) does not apply unless your company has 15 or more employees. But that doesn’t mean your business cannot be ADA-compliant. If you have customers using wheelchairs or other assistive devices entering your place of business, you still want to accommodate them.
+Pregnancy Discrimination Act – 15 employees
+    - The Pregnancy Discrimination Act (PDA) prohibits discrimination on the basis of pregnancy, childbirth and other related medical issues. If you have five or more employees, California requires you to provide Pregnancy Disability Leave to any expectant mothers working for you. This does not just include the legally mandated time-off. It could also include additional disability leave for complicated pregnancies.
+ADEA – 20 Employees
+    - The Age Discrimination in Employment Act (ADEA) prohibits employers from discriminating against people age 40 or older in hiring, firing, layoffs, wages, and benefits.
+COBRA – 20 employees
+    - The Consolidated Omnibus Budget Reconciliation Act (COBRA) gives workers and their families who lose their health benefits the right to choose to continue group health benefits provided by their group health plan for limited periods of time under certain circumstances such as voluntary or involuntary job loss, reduction in the hours worked, transition between jobs, death, divorce, and other life events. Qualified individuals may be required to pay the entire premium for coverage up to 102 percent of the cost to the plan.
+FMLA – 50 Employees
+    - The Family Medical Leave Act (FMLA) grants job-protected unpaid leave to certain workers in companies with at least 50 employees who work within 75 miles of the work site. Be careful here – if you have fewer than 50 employees and want to offer the same type of benefits to your employees, you will be required to follow the FMLA to the letter even though you are not required to provide the leave. Several states have enacted laws similar to the FMLA that may apply employers with fewer than 50 employees.
+
+# Resources to review
+
+https://webapps.dol.gov/elaws/FirstStep/
+https://hr.blr.com/HR-topics
