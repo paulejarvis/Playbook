9bff1206f6b88ade14df98230e9034fb443c6972
Paul Jarvis <paul.e.jarvis@gmail.com>
2018-12-02 20:19:48 -0800

    push from remote tool; pull in from github

--- start_here_script.py
+++ start_here_script.py
@@ -4,7 +4,7 @@ print("Welcome to Playbook. We're going to ask some questions to understand\
 # gathering data on organization and assigning it to variables
 
 name = raw_input("What is the name of your organization?")
-inc_date = "test"
+inc_date = "" ''
 
 
 
