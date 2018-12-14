e9f4a3564bb6b179b3ab3821dcda05a8c410f870
Paul Jarvis <paul.e.jarvis@gmail.com>
2018-12-14 11:46:31 -0800

    working out windows issues

--- Queries.sh
+++ Queries.sh
@@ -1,11 +1,11 @@
-# move to python
+#!/bin/sh
 
 echo What are you searching for?
 
 read search_term
 echo You are interesting in knowing more about processes involving $search_term
 
-echo "Here are the process steps involving $search_term:\n"
+echo Here are the process steps involving $search_term:
 
 grep -B 6 -A 4 $search_term * | grep process | sed -e 's/<process>/ /g'
 
