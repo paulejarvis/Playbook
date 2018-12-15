7c08c6f1f451fd0067b9faf717a27e4322f7e094
Paul Jarvis <paul.e.jarvis@gmail.com>
2018-12-14 21:17:32 -0800

    adapted a few things

--- Process/Queries.sh
+++ Process/Queries.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
-
 echo What are you searching for?
-
 read search_term
 echo You are interesting in knowing more about processes involving $search_term
-
 echo Here are the process steps involving $search_term:
-
 grep -B 6 -A 4 $search_term * | grep process | sed -e 's/<process>/ /g'
 
 # replace the star with a user input directory to target the search to a
--- Process/Unit_Tests.py
+++ Process/Unit_Tests.py
@@ -1,3 +1,21 @@
 # put unit tests here
 
 # unit tests should be the business controls that are encoded
+
+
+
+Department of Labor / Office of Federal Contract Compliance Programs (OFCCP applies to vendors doing business with the government)
+
+* must be published on careers page
+* keep a copy of the request form to validate that we gathered in the int
+
+Question
+* where is the need coming from
+* FT / contractor
+* desired time to hire
+* Job description and specific skills
+* Location and travel
+* hiring process
+
+
+https://news.ycombinator.com/item?id=18585677
